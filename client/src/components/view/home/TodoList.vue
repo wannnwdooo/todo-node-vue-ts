cff835147e97db41258098c5d4003540f184a043
wannnwdooo <wannnwdooo@gmail.com>
2023-04-10 20:44:37 +0300

    fix: delete todo, add: title for an empty list

--- client/src/components/view/home/TodoList.vue
+++ client/src/components/view/home/TodoList.vue
@@ -1,15 +1,20 @@
 <template>
   <ul>
-    <TodoItem :data="item" v-for="item in store.getters['todo/allTodo']" class="todo-item" />
+    <template v-if="todoItems.length">
+      <TodoItem :data="item" :key="item._id" v-for="item in todoItems" class="todo-item" />
+    </template>
+    <li class="todo-title" v-else>This is empty</li>
   </ul>
 </template>
 
 <script lang="ts" setup>
 import TodoItem from '@/components/view/home/TodoItem.vue'
-import { onMounted } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useStore } from 'vuex'
 
 const store = useStore()
+
+const todoItems = computed(() => store.getters['todo/allTodo'])
 
 onMounted(async () => {
   await store.dispatch('todo/getAllTodo')
@@ -25,6 +30,14 @@ ul {
     &:not(:last-child) {
       margin-bottom: 1rem;
     }
+  }
+  .todo-title {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    color: #272727;
+    font-weight: 700;
+    font-size: 1.2rem;
   }
 }
 </style>
